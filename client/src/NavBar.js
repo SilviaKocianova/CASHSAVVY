f01ad38fa9e0fa4aa73fa8ea5ba3773a6b8a3e4a
Silvia Kociánová <sissi.kocianova@gmail.com>
2024-05-15 16:15:30 +0200

    icons

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -9,17 +9,17 @@ const NavBar = () => {
       <ul className="navbar-list">
         <li className="navbar-item">
           <Link to="/budget">
-            <img src="D:\PROGRAMMING\cashsavvy graphics\figma\icon_brain.png" alt="Budget" />
+            <img src="/icon_brain.png" alt="Budget" />
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/saving-goals">
-            <img src="D:\PROGRAMMING\cashsavvy graphics\figma\icon_pig.png" alt="Saving Goals" />
+            <img src="/icon_pig.png" alt="Saving Goals" />
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/expense">
-            <img src="D:\PROGRAMMING\cashsavvy graphics\figma\icon_money.png" alt="Expense" />
+            <img src="/icon_money.png" alt="Expense" />
           </Link>
         </li>
       </ul>
